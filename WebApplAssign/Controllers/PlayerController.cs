19d98d46c0bf4ee2d35021bcb5ae25ad6730e622
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2024-01-15 16:21:20 +0530

    Assignment

---
+++ WebApplAssign/Controllers/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplAssign.Models
+{
+    public class PlayerController : Controller
+    {
+        static List<Player> players = new List<Player>()
+        {
+            new Player() {PId=1,PName="Viratkohli",PCountry="India",PType="Batsman" },
+            new Player() {PId=2,PName="MS Dhoni",PCountry="India",PType="WicketKeeper" },
+            new Player() {PId=3,PName="Bumrah",PCountry="India",PType="Bowler" },
+            new Player() {PId=4,PName="Jageja",PCountry="India",PType="All-Rounder" },
+
+        };
+        public IActionResult Index()
+        {
+            return View(players);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Player());
+        }
+        [HttpPost]
+        public IActionResult Create(Player pro)
+        {
+            if (ModelState.IsValid)
+            {
+                players.Add(pro);
+                return RedirectToAction("Index");
+            }
+            return View(pro);
+        }
+    }
+}
---
+++ WebApplAssign/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplAssign.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
